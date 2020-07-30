c243a1f427eab5d3bea3b5492d78a62dc18fac4b
Muhammad Asif <moh.asif4347@gmail.com>
2020-07-30 13:22:59 +0500

    dynamic settings added in setting_dicts.py

--- Home/urls.py
+++ Home/urls.py
@@ -3,14 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('pattern_picker', pattern_picker, name='pattern_picker'),
-    path('delay_slider', delay_slider, name='delay_slider'),
-    path('random', random, name='random'),
-    path('red', red, name='red'),
-    path('green', green, name='green'),
-    path('blue', blue, name='blue'),
-    path('alpha', alpha, name='alpha'),
-    path('size', size, name='size'),
-    path('random_decay', random_decay, name='random_decay'),
-    path('trail_decay', trail_decay, name='trail_decay'),
+    path('render_settings', render_setting, name='render_settings'),
+    path('get_form_values', get_form_values, name='get_form_values'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from getenv import env
 import os
+from .setting_dicts import drop_down, random, slider_values
 
 
 def get_val_from_env(key_name: str):
@@ -16,189 +18,36 @@ def index(request):
     # if you want to get environment variable
     # env_variable = get_val_from_env('my_env')
     # print(env_variable)
-    drop_down = [
-        {
-            'label': 'Color Wipe',
-            'value': 'CW'
-        },
-        {
-            'label': 'Color Display',
-            'value': 'CD'
-        }
-    ]
-    random = True  # set values as per you want below if random
-    random_decay = True
-    slider_values = {
-        'delay': {
-            'min': 0,
-            'max': 100,
-            'current': 23
-        },
-        'red': {
-            'min': 0,
-            'max': 255,
-            'current': 23
-        },
-        'green': {
-            'min': 0,
-            'max': 255,
-            'current': 23
-        },
-        'blue': {
-            'min': 0,
-            'max': 255,
-            'current': 40
-
-        },
-        'alpha': {
-            'min': 0,
-            'max': 255,
-            'current': 23
-        },
-        'size': {
-            'min': 0,
-            'max': 100,
-            'current': 50
-        },
-        'trail_decay': {
-            'min': 0,
-            'max': 100,
-            'current': 10
-        }
-
-    }
 
     context = {
         'sliders': slider_values,
         "drop_down": drop_down,
         'random': random,
-        'random_decay': random_decay,
-        'show_metior': get_val_from_env('show_metior')
     }
     return render(request, 'home/index.html', context=context)
 
 
-def pattern_picker(request):
-    value = request.GET.get('value')
-    print(value)
+def render_setting(request):
+    print(request.GET)
+    setting_name = request.GET.get('setting_name', '')
+    setting_html = ''
+    for setting in drop_down:
+        if setting['Name'].__contains__(setting_name):
+            setting_html = render_to_string('home/setting_form.html', setting)
+            response = {
+                'success': True,
+                'value': setting_html,
+            }
+            return JsonResponse(response, safe=False)
+    return JsonResponse({'success': False, 'value': None})
+
+
+def get_form_values(request):
+    values = request.GET.dict()
+    print(values)  # values in dict
     # Your code here
     response = {
         'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def delay_slider(request):
-    value = int(request.GET.get('value'))
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def random(request):
-    value = request.GET.get('value')
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def red(request):
-    value = int(request.GET.get('value'))
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def green(request):
-    value = int(request.GET.get('value'))
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def blue(request):
-    value = int(request.GET.get('value'))
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def alpha(request):
-    value = int(request.GET.get('value'))
-    print(value)
-    # Your code here
-    # just as example to show metior
-    if value > 30:
-        os.environ['show_metior'] = "True"  # envoirnment accpets only strings
-    else:
-        os.environ['show_metior'] = "False"
-
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def size(request):
-    value = int(request.GET.get('value'))
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def random_decay(request):
-    value = request.GET.get('value')
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
-    }
-    return JsonResponse(response, safe=False)
-
-
-def trail_decay(request):
-    value = int(request.GET.get('value'))
-    print(value)
-    # Your code here
-    response = {
-        'success': True,
-        'value': value,
-        'show_metior': get_val_from_env('show_metior')
+        'value': values,
     }
     return JsonResponse(response, safe=False)
